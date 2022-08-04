cec61509652883ed5d3c2475dbe4e471008d796f
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-08-04 16:01:25 +0200

    Add readme, Add loading screen

--- src/ListApi.js
+++ src/ListApi.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 export default function ListApi() {
-  const [guest, setGuest] = useState([]);
+  const [guest, setGuest] = useState([]); // guest list
   const [loading, setLoading] = useState(true);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -9,21 +9,25 @@ export default function ListApi() {
   // get all guests from api
   const baseUrl = 'http://localhost:4000';
 
+  async function getFullGuestList() {
+    const response = await fetch(`${baseUrl}/guests`);
+    const allGuests = await response.json();
+    setGuest(allGuests);
+    setLoading(false);
+    console.log('fetching guest list');
+  }
+
+  // fetch guest list
   useEffect(() => {
-    async function getFullGuestList() {
-      const response = await fetch(`${baseUrl}/guests`);
-      const allGuests = await response.json();
-      setGuest(allGuests);
-      setLoading(false);
-    }
     getFullGuestList().catch(() =>
       console.log('fetching all guests went wrong'),
     );
-  }, [guest]);
+  }, []);
 
   // adding guest to array
   async function addNewGuest(e) {
     e.preventDefault();
+    console.log('A');
     const response = await fetch(`${baseUrl}/guests`, {
       method: 'POST',
       headers: {
@@ -34,11 +38,14 @@ export default function ListApi() {
         lastName: lastName,
       }),
     });
+    console.log('B');
     const createdGuest = await response.json();
     console.log(createdGuest);
     setGuest([...guest], createdGuest);
+    getFullGuestList().catch(() => console.log('adding new guest went wrong'));
+    setFirstName('');
+    setLastName('');
   }
-  // addNewGuest().catch(() => console.log('adding guest went wrong'));
 
   // deleting from array
   async function deleteGuest(id) {
@@ -50,10 +57,13 @@ export default function ListApi() {
       return i.id !== deletedGuest.id;
     });
     setGuest(newGuestList);
+    getFullGuestList().catch(() =>
+      console.log('deleting from guests went wrong'),
+    );
   }
 
   // toggles attending/not attending
-  async function isAttending(id, value) {
+  async function changeAttendingStatus(id, value) {
     const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'PUT',
       headers: {
@@ -66,6 +76,9 @@ export default function ListApi() {
       return i.id !== updatedGuest.id;
     });
     setGuest([...guest], updatedGuestList);
+    getFullGuestList().catch(() =>
+      console.log('changing attending status went wrong'),
+    );
   }
 
   // reset
@@ -74,46 +87,8 @@ export default function ListApi() {
     <h1>loading...</h1>
   ) : (
     <div data-test-id="guest">
-      <div>
-        {guest.map((guestList) => {
-          return (
-            <ul key={guestList.id}>
-              <li>
-                {guestList.firstName} {guestList.lastName}
-              </li>
-              <li>
-                <label>
-                  <input
-                    aria-label={`Attending status ${guestList.firstName} ${guestList.lastName}`}
-                    type="checkbox"
-                    checked={guestList.attending}
-                    onChange={() => {
-                      isAttending(guestList.id, guestList.attending).catch(() =>
-                        console.log('adding guest went wrong'),
-                      );
-                    }}
-                  />
-                  {guestList.attending === true ? 'attending' : 'not attending'}
-                </label>
-              </li>
-              <button
-                aria-label={`Remove ${guestList.firstName} ${guestList.lastName}`}
-                onClick={() => {
-                  deleteGuest(guestList.id).catch(() =>
-                    console.log('adding guest went wrong'),
-                  );
-                  document.form.reset();
-                }}
-              >
-                remove guest
-              </button>
-            </ul>
-          );
-        })}
-      </div>
-
       <h2>New Guest:</h2>
-      <form name="form">
+      <form name="form" onSubmit={addNewGuest}>
         <label>
           First Name
           <input
@@ -127,20 +102,61 @@ export default function ListApi() {
           <input
             value={lastName}
             onChange={(e) => setLastName(e.currentTarget.value)}
-            // onKeyDown={(e) => {
-            //   if (e.key === 'Enter') {
-            //     e.preventDefault();
-            //     addNewGuest().catch(() =>
-            //       console.log('adding guest went wrong'),
-            //     );
-            // document.form.reset();
-            //   }
-            // }}
           />
         </label>
         <br />
         <button onClick={addNewGuest}>Add new guest</button>
       </form>
+      <div>
+        {/* check if there are guests, show list */}
+        {guest.length === 0 ? (
+          <div>please add guests to your list :)</div>
+        ) : (
+          <div>
+            <h2>Guests:</h2>
+            {guest.map((guestList) => {
+              return (
+                <ul key={guestList.id}>
+                  <li>
+                    {guestList.firstName} {guestList.lastName}
+                  </li>
+                  <li>
+                    <label>
+                      <input
+                        aria-label={`Attending status ${guestList.firstName} ${guestList.lastName}`}
+                        type="checkbox"
+                        checked={guestList.attending}
+                        onChange={() => {
+                          changeAttendingStatus(
+                            guestList.id,
+                            guestList.attending,
+                          ).catch(() =>
+                            console.log('changing attending status went wrong'),
+                          );
+                        }}
+                      />
+                      {guestList.attending === true
+                        ? 'attending'
+                        : 'not attending'}
+                    </label>
+                  </li>
+                  <button
+                    aria-label={`Remove ${guestList.firstName} ${guestList.lastName}`}
+                    onClick={() => {
+                      deleteGuest(guestList.id).catch(() =>
+                        console.log('adding guest went wrong'),
+                      );
+                      document.form.reset();
+                    }}
+                  >
+                    remove guest
+                  </button>
+                </ul>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
